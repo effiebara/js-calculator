169087c5e394019abecb4844654ebc9d89b14654
Effie Bara <effiebara@gmail.com>
2021-08-07 17:16:41 +0300

    created a calculator

---
+++ calculator.js
@@ -0,0 +1,31 @@
+let activeOperant= "";
+
+function activateField(option){
+    // if (option=="a") {
+    //     activeOperant="a";
+    // } else {
+    //   activeOperant="b";
+    // }
+    activeOperant=option;
+    
+}
+
+function addNumber(buttonNumber){
+    // create the active operant id
+    let operantId = "operant"+activeOperant;
+    document.getElementById(operantId).value += buttonNumber;
+}
+
+function add(){
+    let op1=document.getElementById("operanta").value;
+    op1=Number(op1);
+    let op2=document.getElementById("operantb").value;
+    op2=Number(op2);
+    document.getElementById("result").value = op1+op2;
+}
+
+function cancel(){
+    document.getElementById("operanta").value = "";
+    document.getElementById("operantb").value = "";
+    document.getElementById("result").value = "";
+}
