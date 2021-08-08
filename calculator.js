fccf167718f9efb22a084f560f7551255e4931e4
George Baras <georgebaras@gmail.com>
2021-08-08 14:25:00 +0300

    add  all operations

--- calculator.js
+++ calculator.js
@@ -29,3 +29,36 @@ function cancel(){
     document.getElementById("operantb").value = "";
     document.getElementById("result").value = "";
 }
+
+function subtract(){
+    let op1=document.getElementById("operanta").value;
+    op1=Number(op1);
+    let op2=document.getElementById("operantb").value;
+    op2=Number(op2);
+    document.getElementById("result").value = op1-op2;   
+}
+
+function multiply(){
+    let op1=document.getElementById("operanta").value;
+    op1=Number(op1);
+    let op2=document.getElementById("operantb").value;
+    op2=Number(op2);
+    document.getElementById("result").value = op1*op2; 
+}
+
+function division(){
+    let op1=document.getElementById("operanta").value;
+    op1=Number(op1);
+    let op2=document.getElementById("operantb").value;
+    op2=Number(op2);
+    document.getElementById("result").value = op1/op2;
+}
+
+function cancelLastOption() {
+    if (activeOperant=='a') {
+        document.getElementById("operanta").value = "";
+    } else  {
+        document.getElementById("operantb").value = "";
+    }
+    
+}
